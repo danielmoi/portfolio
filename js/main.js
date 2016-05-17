a8ba5aa49f0660235fb53a9f987a525e4dfdad7a
danielmoi <daniel@ampersandmoi.com>
2016-05-17 17:16:14 +1000

    Organise JS into sections and improve comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-var currentSection = '';
+/////////////////////////////////////////////////////////////////////////////
 
+// NAV BAR
+
+// Nav Event Handlers
+$('.nav__list-item').on('vclick', function(e) {
+  var section = $($(this).children()[0]).attr('data-section');
+  currentSection = section;
+  $('.main__container').fadeOut(200).delay(200);
+  $('#' + section).fadeIn(300);
+});
+
+$('.nav__toggle').on('vclick', function(e) {
+  $('.nav').toggle();
+});
+
+$('.nav__list-item').on('vclick', function(e) {
+  if ($('.nav__list-item').css('display') === 'block') {
+    $('.nav').toggle();
+  }
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////
+
+// PROJECT SECTION
+
+// Counters
+var currentSection = '';
+var indexCurrentProject = 0;
+
+// Project Data
 var arrProjects = [{
   name: 'Musical Monsters',
   img: 'img/musical-monsters.png',
@@ -14,45 +45,21 @@ var arrProjects = [{
   desc: 'Events Management'
 }];
 
-$('.nav__list-item').on('vclick', function(e) {
-  var section = $($(this).children()[0]).attr('data-section');
-  currentSection = section;
-  $('.main__container').fadeOut(200).delay(200);
-  console.log(section);
-  $('#' + section).fadeIn(300);
-});
-
-
-
-$('.nav__toggle').on('vclick', function(e) {
-  $('.nav').toggle();
-});
-
-$('.nav__list-item').on('vclick', function(e) {
-  if ($('.nav__list-item').css('display') === 'block') {
-    $('.nav').toggle();    
-  }
-});
-
-// $('.project__container').on('vclick', function(e) {
-//   console.log($('.project__image').attr('src', arrImages[1]));
-// });
-
+// Element Selectors
 $title = $('.project__title');
 $description = $('.project__description');
 $img = $('.project__image');
 
+// Populate project with data
 var populateProject = function(index) {
   var objCurrentProject = arrProjects[index];
   $title.text(objCurrentProject.name);
   $description.text(objCurrentProject.desc);
   $img.attr('src', objCurrentProject.img);
-
 };
 
-var indexCurrentProject = 0;
 
-// PREVIOUS PROJECT
+// Previous project
 var projectNext = function() {
   indexCurrentProject += 1;
   if (indexCurrentProject === arrProjects.length) {
@@ -61,7 +68,7 @@ var projectNext = function() {
   populateProject(indexCurrentProject);
 };
 
-// NEXT PROJECT
+// Next project
 var projectPrevious = function() {
   indexCurrentProject -= 1;
   if (indexCurrentProject < 0) {
@@ -71,22 +78,22 @@ var projectPrevious = function() {
 };
 
 
-// CLICK RIGHT
+// Click Right
 $('.project__overlay--right').on('vclick', function() {
   projectNext();
 });
 
-// CLICK LEFT
+// Click Left
 $('.project__overlay--left').on('vclick', function() {
   projectPrevious();
 });
 
-// SWIPE RIGHT
+// Swipe Right
 $('.project__container').on('swiperight', function() {
   projectNext();
 });
 
-// SWIPE LEFT
+// Swipe Left
 $('.project__container').on('swipeleft', function() {
   projectPrevious();
 });
