30a6c761680728007c202d3089c04121645ef32f
danielmoi <daniel@ampersandmoi.com>
2016-05-17 14:25:31 +1000

    Reference jQuery mobile correctly

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+var currentSection = '';
+
 $('.nav__list-item').on('vclick', function(e) {
-  // console.log($(this));
-  // console.log($(this).children()[0].attr('data-section'));
-  // console.log($(this).children()[0]);
   var section = $($(this).children()[0]).attr('data-section');
+  currentSection = section;
   $('.main__container').fadeOut(200).delay(200);
   console.log(section);
   $('#' + section).fadeIn(300);
