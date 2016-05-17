0a6cde71e13646120487919473692cee64ba7c66
danielmoi <daniel@ampersandmoi.com>
2016-05-17 13:27:36 +1000

    Fade nav sections with jQuery

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.nav__list-item').on('click', function(e) {
+  // console.log($(this));
+  // console.log($(this).children()[0].attr('data-section'));
+  // console.log($(this).children()[0]);
+  var section = $($(this).children()[0]).attr('data-section');
+  $('.main__container').fadeOut(200).delay(200);
+  console.log(section);
+  $('#' + section).fadeIn(300);
+});
