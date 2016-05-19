582e75c79feddfc71d803b1846bdfaa57b156d1d
danielmoi <daniel@ampersandmoi.com>
2016-05-19 15:07:47 +1000

    Move monsters 3 and 4

--- js/monster.js
+++ js/monster.js
@@ -11,39 +11,77 @@ var wheel3 = $('#wheel-3');
 var wheel4 = $('#wheel-4');
 var wheel5 = $('#wheel-5');
 var wheel6 = $('#wheel-6');
+var wheel7 = $('#wheel-7');
+var wheel8 = $('#wheel-8');
 
 // Move monster 1
-var tlmove1 = new TimelineMax({
+var tlMove1 = new TimelineMax({
   onComplete: function() {
-    console.log('tlmove1 complete');
+    console.log('tlMove1 complete');
     tlWheelA.pause();
-    tlmove2.play();
+    tlMove2.play();
     tlWheelB.play();
   }
 });
-tlmove1.to($m1, 3, {
+tlMove1.to($m1, 3, {
   x: -250,
   ease: Power0.easeNone
 });
 
 // Move monster 2
-var tlmove2 = new TimelineMax({
+var tlMove2 = new TimelineMax({
   paused: true,
   onComplete: function() {
-    console.log('tlmove2 complete');
+    console.log('tlMove2 complete');
     tlWheelB.pause();
+    tlMove3.play();
+    tlWheelC.play();
   }
 });
-tlmove2.to($m2, 2, {
+tlMove2.to($m2, 2, {
   y: 151,
   ease: Power0.easeNone
 });
-tlmove2.to($m2, 3, {
+tlMove2.to($m2, 3, {
   x: 250,
   ease: Power0.easeNone
 });
 
+// Move monster 3
+var tlMove3 = new TimelineMax({
+  paused: true,
+  onComplete: function() {
+    console.log('tlMove3 complete');
+    tlWheelC.pause();
+    tlMove4.play();
+    tlWheelD.play();
+  }
+});
+tlMove3.to($m3, 2, {
+  y: 195,
+  ease: Power0.easeNone
+});
+tlMove3.to($m3, 3, {
+  x: -250,
+  ease: Power0.easeNone
+});
 
+// Move monster 4
+var tlMove4 = new TimelineMax({
+  paused: true,
+  onComplete: function() {
+    console.log('tlMove4 complete');
+    tlWheelD.pause();
+  }
+});
+tlMove4.to($m4, 2, {
+  y: 146,
+  ease: Power0.easeNone
+});
+tlMove4.to($m4, 3, {
+  x: 250,
+  ease: Power0.easeNone
+});
 
 // Move first pair of wheels
 var tlWheelA = new TimelineMax();
@@ -54,8 +92,27 @@ tlWheelA.to([wheel1, wheel2], 1, {
   ease: Linear.easeNone
 });
 
+// Move second pair of wheels
 var tlWheelB = new TimelineMax({ paused: true });
 tlWheelB.to([wheel3, wheel4], 1, {
+  rotation: 360,
+  transformOrigin: '50% 50%',
+  repeat: -1,
+  ease: Linear.easeNone
+});
+
+// Move third pair of wheels
+var tlWheelC = new TimelineMax({ paused: true });
+tlWheelC.to([wheel5, wheel6], 1, {
+  rotation: -360,
+  transformOrigin: '50% 50%',
+  repeat: -1,
+  ease: Linear.easeNone
+});
+
+// Move fourth pair of wheels
+var tlWheelD = new TimelineMax({ paused: true });
+tlWheelD.to([wheel7, wheel8], 1, {
   rotation: 360,
   transformOrigin: '50% 50%',
   repeat: -1,
