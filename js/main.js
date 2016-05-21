76610c284c7ba0392d5da625f5373e516bfbd18c
danielmoi <daniel@ampersandmoi.com>
2016-05-21 18:21:16 +1000

    Tidy code for nav

--- js/main.js
+++ js/main.js
@@ -3,37 +3,22 @@
 // NAV BAR
 
 // Nav Event Handlers
-$('.nav__list-item').on('mouseup touchend', function(e) {
-  console.log('nav__list-item clicked' );
+$('.nav__list-item').on('vclick', function(e) {
   $('.nav__list-item').removeClass('nav__active');
   $(this).addClass('nav__active');
-
-  // location.href=$(this).children().attr('href');
-  // e.stopPropagation();
-  // return true;
 });
 
-$('a').on('click', function(e) {
-  console.log('a clicked');
-  // e.stopImmediatePropagation();
-  console.log('a clicked line 2');
+$('.nav__list-item').on('click', function(e) {
   if ($('.nav__list-item').css('display') === 'block') {
     $('.nav').hide();
-    return true;
   }
 });
 
 $('.nav__toggle').on('vclick', function(e) {
   $('.nav').toggle();
-  console.log('nav toggle clicked');
 });
 
 
-// $('a[data-section]').on('vclick', function(e) {
-//   e.stopPropagation();
-//   console.log('a clicked');
-//   $('.nav').hide();
-// });
 ////////////////////////////////////////////////////////////////////////////
 
 // HOME
