ba987c05efe1c9c9b06207baa7e3be4364c5816c
danielmoi <daniel@ampersandmoi.com>
2016-05-28 12:13:57 +1000

    Add header and nav to learn

--- learn/header.php
+++ learn/header.php
@@ -33,3 +33,50 @@
 
 </head>
 <body>
+
+  <header class="header">
+    <div class="container">
+      <h1 class="header__title">
+
+        <a href="/">
+          <img class="header__logo" src="img/danielmoi-name.svg" alt="Daniel Moi"/>
+
+        </a>
+      </h1>
+
+
+    </div>
+  </header>
+
+
+  <nav class="nav">
+    <div class="container">
+      <ul class="nav__list">
+        <li>
+          <a class="nav__list-item nav__active" data-section="home" href="#">Home</a>
+        </li>
+
+        <li>
+          <a class="nav__list-item" data-section="about" href="#about">About Me</a>
+        </li>
+
+        <li>
+          <a class="nav__list-item" data-section="projects" href="#projects">Projects</a>
+        </li>
+
+        <li>
+          <a class="nav__list-item" data-section="design" href="#design">Design</a>
+        </li>
+
+        <li>
+          <a class="nav__list-item" data-section="learn" href="/learn">Learn</a>
+        </li>
+
+        <li>
+          <a class="nav__list-item" data-section="contact" href="#contact">Say hello!</a>
+        </li>
+
+      </ul>
+    </div>
+  </nav>
+  <div class="nav__toggle">+</div>
