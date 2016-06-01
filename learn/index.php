9bd468808e4f48adbc8716939be76f2b984cbf14
danielmoi <daniel@ampersandmoi.com>
2016-06-01 14:32:48 +1000

    Refactor php and html for Learn homepage

--- learn/index.php
+++ learn/index.php
@@ -1,12 +1,4 @@
-<?php include "header-learn-start.php"; ?>
-<?php include "header-learn-end.php"; ?>
-
-<div class="main__container">
-  <h1>Learn</h1>
-
-  <h2>Jargon</h2>
-  <h2>CSS</h2>
-  <h2>JavaScript</h2>
-</div>
-
-<?php include "footer.php"; ?>
+<?php include "header-learn-start.html"; ?>
+<?php include "header-learn-end.html"; ?>
+<?php include "learn.html"; ?>
+<?php include "footer.html"; ?>
