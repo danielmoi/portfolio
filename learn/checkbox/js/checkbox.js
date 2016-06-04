26034531736aee56d84ef0595a2d33f23187908e
danielmoi <daniel@ampersandmoi.com>
2016-06-04 12:19:43 +1000

    Add explanation to checkbox retrieval That we receive, and iterate through, an array

--- learn/checkbox/js/checkbox.js
+++ learn/checkbox/js/checkbox.js
@@ -3,9 +3,9 @@
 $('.btn--cb4').on('click', function() {
   var arrCB = [];
 
-  // using $.map will make arrCB a jQuery object
+  // using $.map will make arrCB a jQuery object, which makes it difficult to display
   // arrCB = $('input[name="cb4"]:checked').map(function(el) {
-  //   return $(this).val();
+    // return $(this).val();
   // });
 
   $('input[name="cb4"]:checked').each(function(index, el) {
