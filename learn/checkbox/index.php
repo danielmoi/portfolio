977beae7c026026b839d7f747a63b721c0b10e85
danielmoi <daniel@ampersandmoi.com>
2016-06-04 10:26:55 +1000

    Start Checkbox tute, add Step 1

---
+++ learn/checkbox/index.php
@@ -0,0 +1,4 @@
+<?php include '../header-learn-start.html'; ?>
+<?php include '../header-learn-end.html'; ?>
+<?php include 'checkbox.html'; ?>
+<?php include '../footer-learn.html'; ?>
---
+++ learn/checkbox/js/checkbox.js
@@ -0,0 +1,7 @@
+// $('[data-section="select"]').addClass('nav__active');
+
+$('.btn--select2').on('click', function() {
+  $('.select2-value').text($('select[name="select2"]').val());
+});
+
+console.log('checkbox');
