7d2ce015349d9ea42ea86f9a0455a7adeaa903c9
danielmoi <daniel@ampersandmoi.com>
2016-06-04 12:05:54 +1000

    Add steps including value retrieval to checkbox tute

--- learn/checkbox/js/checkbox.js
+++ learn/checkbox/js/checkbox.js
@@ -1,7 +1,18 @@
 // $('[data-section="select"]').addClass('nav__active');
 
-$('.btn--select2').on('click', function() {
-  $('.select2-value').text($('select[name="select2"]').val());
-});
+$('.btn--cb4').on('click', function() {
+  var arrCB = [];
 
-console.log('checkbox');
+  // using $.map will make arrCB a jQuery object
+  // arrCB = $('input[name="cb4"]:checked').map(function(el) {
+  //   return $(this).val();
+  // });
+
+  $('input[name="cb4"]:checked').each(function(index, el) {
+    arrCB.push(el.value);
+  });
+
+  $('.cb4-value').text(arrCB.join(', '));
+
+  console.log(arrCB);
+});
